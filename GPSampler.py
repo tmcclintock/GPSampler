2a86648c57f6a915a04e4bd7aa4609e8ddc10372
Tom McClintock <tmcclintock@email.arizona.edu>
2017-08-02 11:07:12 -0700

    It's working fine now.

--- GPSampler.py
+++ GPSampler.py
@@ -9,13 +9,26 @@ class GPSampler(object):
         """Create the latin-hypercube for the parameter space.
         """
         import lhsmdu
-        dim = len(self.params)
+        self.dim = len(self.params)
+        self.N   = N
         print "here"
-        self.LHC = np.array(lhsmdu.sample(dim, N)).T
+        self.LHC = np.array(lhsmdu.sample(self.dim, self.N)).T
         print "now here"
 
     def evaluate_func(self):
         self.Y = np.array([self.func(np.array(x)) for x in self.LHC])
+
+    def train_GP(self):
+        import george
+        lguess = (np.max(self.LHC, 0) - np.min(self.LHC, 0))/self.N
+        kernel = 1.0*george.kernels.ExpKernel(metric=lguess, ndim=self.dim) + george.kernels.WhiteKernel(1.0, ndim=self.dim)
+        gp = george.GP(kernel)
+        gp.optimize(self.LHC, self.Y)
+        print gp.kernel
+        self.GP = gp
+
+    def predict(self, t):
+        return self.GP.predict(self.Y, t)
 
 if __name__ == "__main__":
     #Let's say we have some log-likelihood function
@@ -25,15 +38,36 @@ if __name__ == "__main__":
     gps = GPSampler(llike, [[0,1],[0,1]])
     gps.create_LH()
     gps.evaluate_func()
+    gps.train_GP()
 
     import matplotlib.pyplot as plt
-    #x = np.linspace(0, 1, 50)
+    x = np.linspace(-0.5, 1.5, 50)
     #plt.plot(x, [llike(xi) for xi in x], c='k')
+    #mu, cov = gps.predict(x)
+    #plt.plot(x, mu, c='r')
+    #plt.scatter(gps.LHC[:,0], gps.Y, c='b')    
+    #err = np.sqrt(np.diag(cov))
+    #plt.fill_between(x, mu-err, mu+err, color='r', alpha=0.2)
     print gps.LHC.shape
     print gps.Y.shape
-    #plt.scatter(gps.LHC[:,0], gps.Y, c='b')
     from mpl_toolkits.mplot3d import Axes3D
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(gps.LHC[:,0], gps.LHC[:,1], gps.Y)
+
+    Nd = 50
+    X0 = np.linspace(np.min(x), np.max(x), Nd)
+    X1 = np.linspace(np.min(x), np.max(x), Nd)
+    D0, D1 = np.meshgrid(X0, X1)
+    X = np.array([D0.flatten(), D1.flatten()])
+    mu = np.ones_like(D0)
+    for i in range(len(X0)):
+        for j in range(len(X1)):
+            Xij = np.atleast_2d([X0[i], X1[j]])
+            muij, cov = gps.predict(Xij)
+            mu[i,j] = muij
+    print mu.shape
+    count = 20
+    ax.plot_wireframe(D0, D1, mu, rcount=count, ccount=count, color='r', alpha=0.2)
+
     plt.show()
